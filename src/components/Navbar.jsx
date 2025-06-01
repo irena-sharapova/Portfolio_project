f49e98b2d8e148c7813ec8ce8fc0152ccdb96329
Irina Sharapova <ir.ur.smirnova@gmail.com>
2025-06-01 13:05:39 -0400

    Add style to elements for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FaInstagram } from 'react-icons/fa';
 import '../CSS/Navbar.css';
 
 
@@ -12,12 +13,21 @@ function MyNavbar() {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="ms-auto gap-4">
                         <Nav.Link as={Link} to="/">Home</Nav.Link>
                         <Nav.Link as={Link} to="/portraits">Portraits</Nav.Link>
                         <Nav.Link as={Link} to="/landscapes">Landscapes</Nav.Link>
                         <Nav.Link as={Link} to="/products">Products</Nav.Link>
                         <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                    <Nav.Link
+                        href="https://www.instagram.com/irena_sharapova/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="instagram-icon"
+                        aria-label="Instagram"
+                    >
+                        <FaInstagram size={24} color="#333" />
+                    </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
