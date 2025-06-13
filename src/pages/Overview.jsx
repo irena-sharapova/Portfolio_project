d4f4e7daaf678ae09d4a93b0dade0192730b49ae
Irina Sharapova <ir.ur.smirnova@gmail.com>
2025-06-12 23:00:35 -0400

    Add overview page

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,7 +1,49 @@
+import { useState } from 'react';
+import 'react-image-lightbox/style.css';
+import Lightbox from 'react-image-lightbox';
+import '../CSS/Overview.css'; 
+
+const images = Object.values(
+  import.meta.glob('../assets/Images/images_overview/*.{jpg,jpeg,png}', {
+    eager: true,
+    query: '?url',
+    import: 'default'
+  })
+);
+
 export default function Overview() {
-    return (
-      <div className="p-6">
-        <h1 className="text-3xl font-bold">Overview</h1>
-      </div>
-    );
-  }
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="photo-grid">
+      {images.map((src, index) => (
+        <img
+          key={index}
+          src={src}
+          alt={`Photo ${index + 1}`}
+          className="photo-item"
+          onClick={() => {
+            setPhotoIndex(index);
+            setIsOpen(true);
+          }}
+        />
+      ))}
+
+      {isOpen && (
+        <Lightbox
+          mainSrc={images[photoIndex]}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() =>
+            setPhotoIndex((photoIndex + images.length - 1) % images.length)
+          }
+          onMoveNextRequest={() =>
+            setPhotoIndex((photoIndex + 1) % images.length)
+          }
+        />
+      )}
+    </div>
+  );
+}
