eb8d0cab54cb23f9b69581295390f963f2cc1a5e
Irina Sharapova <ir.ur.smirnova@gmail.com>
2025-06-29 13:39:48 -0400

    Add hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { HashRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>
 );
